4d77d9af0b155b446a9141926455ebdcbe6cb2d0
christopherlinford <linfordc05@gmail.com>
2025-03-18 17:47:01 -0600

    Document object model w2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector(''); // you need to fill in the blank to reference the HTML element that is a unordered list element.
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = 'X';
+li.append(deleteButton);
+list.appent(li);
+
+let el = document.getElementById("close-button");
+console.log(el.ariaLabel); // "Close"
+el.ariaLabel = "Close dialog";
+console.log(el.ariaLabel); // "Close dialog"
