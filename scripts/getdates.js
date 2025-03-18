de6f693404732255c3e73603cc9b384ead902d25
christopherlinford <linfordc05@gmail.com>
2025-03-17 22:06:20 -0600

    home page fix

---
+++ scripts/getdates.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the current year dynamically
+    const currentYear = new Date().getFullYear();
+
+    
+    const copyrightElement = document.getElementById("copyright-year");
+    if (copyrightElement) {
+        copyrightElement.textContent = currentYear;
+    }
+
+    
+    const lastModified = document.lastModified;
+
+    
+    const lastModifiedElement = document.getElementById("last-modified");
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = lastModified;
+    }
+});
